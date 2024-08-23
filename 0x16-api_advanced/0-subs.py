002e54424434d7f1cae8b8c291cd815272f20cc7
Wizsociety <iankimathi14@gmail.com>
2024-08-23 16:40:20 +0300

    api advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,6 +1,21 @@
+#!/usr/bin/python3
+"""
+Queries the Reddit API and returns the number of subscribers for a given subreddit.
+If an invalid subreddit is given, the function returns 0.
+"""
+
 import requests
 
 def number_of_subscribers(subreddit):
+    """
+    Returns the number of subscribers for a given subreddit.
+    
+    Args:
+    subreddit (str): The name of the subreddit.
+    
+    Returns:
+    int: The number of subscribers.
+    """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {"User-Agent": "Custom User Agent"}
     response = requests.get(url, headers=headers, allow_redirects=False)
