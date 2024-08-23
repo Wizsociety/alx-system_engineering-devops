b34182ac7593f8432ce91844324ca03b51ce41a8
Wizsociety <iankimathi14@gmail.com>
2024-08-23 17:04:30 +0300

    api advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -13,15 +13,17 @@ def number_of_subscribers(subreddit):
     """
 
     if subreddit is None or not isinstance(subreddit, str):
+        print("OK")
         return 0
 
     user_agent = {'User-agent': 'Chromium Version 126.0.6478.126'}
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     response = get(url, headers=user_agent)
-    results = response.json()
 
-    try:
+    if response.status_code == 200:
+        results = response.json()
+        print("OK")
         return results.get('data').get('subscribers')
-
-    except Exception:
+    else:
+        print("OK")
         return 0
