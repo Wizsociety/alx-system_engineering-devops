9f5c7d1f6bb6708e80bcffb8c0425f607614ff99
Wizsociety <iankimathi14@gmail.com>
2024-08-23 15:15:51 +0300

    api advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,15 +1,33 @@
-#!/usr/bin/python3
-"subscribers count"
-
 import requests
 
-
 def number_of_subscribers(subreddit):
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    """
+    Queries the Reddit API and returns the number of subscribers for a given subreddit.
+    
+    Args:
+    subreddit (str): The name of the subreddit to query.
+    
+    Returns:
+    int: The number of subscribers if valid, 0 otherwise.
+    """
+    # Define the URL for the subreddit
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    
+    # Set a custom User-Agent to avoid being blocked
     headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    data = response.json()
-    if response.status_code != 200:
+    
+    try:
+        # Make the request to the Reddit API
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        
+        # Check if the response was successful (status code 200)
+        if response.status_code == 200:
+            data = response.json()
+            return data['data']['subscribers']
+        else:
+            # Return 0 for invalid subreddit
+            return 0
+    except requests.RequestException:
+        # Return 0 in case of any request exceptions
         return 0
-    else:
-        return data['data']['subscribers']
+
